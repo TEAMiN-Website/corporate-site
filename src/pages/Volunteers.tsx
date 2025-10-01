248ef7e26de5c1e7ce04b155282e448d5f0751e5
Tim <Noppeney@bvs-bayern.com>
2025-10-01 16:28:30 +0200

    Move text a fraction up

--- src/pages/Volunteers.tsx
+++ src/pages/Volunteers.tsx
@@ -32,7 +32,7 @@ const Volunteers: React.FC = () => {
 
         {/* Text Content */}
         <div className="relative z-10 flex items-end justify-end min-h-[80vh] p-2 lg:p-4">
-          <div className="max-w-xl text-center lg:text-left relative z-10 mb-16 mr-1 lg:mr-2">
+          <div className="max-w-xl text-center lg:text-left relative z-10 mb-20 mr-1 lg:mr-2">
             <h1 className="text-4xl lg:text-6xl font-bold text-white mb-6 leading-tight">
               Lorem Ipsum [Assistants Page]
             </h1>
