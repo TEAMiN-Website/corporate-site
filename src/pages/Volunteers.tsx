23b034ce3377e39cffb677f6be26e3ea9998b84b
Tim <Noppeney@bvs-bayern.com>
2025-10-01 18:12:11 +0200

    Update hero section text to Lorem Ipsum format

--- src/pages/Volunteers.tsx
+++ src/pages/Volunteers.tsx
@@ -31,8 +31,11 @@ const Volunteers: React.FC = () => {
         <div className="relative z-10 flex items-center justify-end min-h-screen p-2 lg:p-4">
           <div className="max-w-xl text-center lg:text-left relative z-10 mr-1 lg:mr-2">
             <div className="mt-64">
-            <h1 className="text-4xl lg:text-6xl font-bold text-white mb-6 leading-tight">
-              Lorem Ipsum [Assistants Page]
+            <h1 className="text-4xl lg:text-6xl font-bold text-white mb-6 leading-tight text-center lg:text-left">
+              Lorem Ipsum<br />
+              Lorem Ipsum<br />
+              Lorem Ipsum<br />
+              Lorem Ipsum
             </h1>
             <div className="flex flex-col sm:flex-row gap-4 justify-center lg:justify-start">
               <Link 
