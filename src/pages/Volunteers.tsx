a7cb15e74a50b939e4bfebfdb5716f26621b99fb
Tim <Noppeney@bvs-bayern.com>
2025-10-01 16:29:11 +0200

    Move background image to the left side

--- src/pages/Volunteers.tsx
+++ src/pages/Volunteers.tsx
@@ -14,7 +14,8 @@ const Volunteers: React.FC = () => {
         <div 
           className="absolute inset-0 bg-cover bg-center"
           style={{
-            backgroundImage: 'url("assistant picture 5-min.jpg")'
+            backgroundImage: 'url("assistant picture 5-min.jpg")',
+            backgroundPosition: 'left center'
           }}
         ></div>
         
