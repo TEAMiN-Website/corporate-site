9bd191d2cfb7fac237482021e58a0186d575857f
Tim <Noppeney@bvs-bayern.com>
2026-03-26 16:45:03 +0700

    Update Footer.tsx

--- src/components/Layout/Footer.tsx
+++ src/components/Layout/Footer.tsx
@@ -35,7 +35,7 @@ const Footer: React.FC = () => {
  <div className="md:col-span-2">
  <div className="flex items-center space-x-2 mb-6">
  <img 
- src="TEAMiN Logo.png" 
+ src="/TEAMiN Logo.png" 
  alt="TEAMiN Logo" 
  className="h-10 w-auto object-contain"
  />
